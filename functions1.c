f34f32c1c6516151d100970ba315ac7e892f9b5d
AhmedMuhammedvip <shizophrenia51@gmail.com>
2023-04-25 23:34:32 +0300

    0x11. C - printf

--- functions1.c
+++ functions1.c
@@ -15,19 +15,19 @@ int print_unsigned(va_list types, char buffer[],
 	int flags, int width, int precision, int size)
 {
 	int i = BUFF_SIZE - 2;
-	unsigned long int num = va_arg(types, unsigned long int);
+	unsigned long int sum = va_arg(types, unsigned long int);
 
-	num = convert_size_unsgnd(num, size);
+	sum = convert_size_unsgnd(sum, size);
 
-	if (num == 0)
+	if (sum == 0)
 		buffer[i--] = '0';
 
 	buffer[BUFF_SIZE - 1] = '\0';
 
-	while (num > 0)
+	while (sum > 0)
 	{
-		buffer[i--] = (num % 10) + '0';
-		num /= 10;
+		buffer[i--] = (sum % 10) + '0';
+		sum /= 10;
 	}
 
 	i++;
@@ -51,22 +51,22 @@ int print_octal(va_list types, char buffer[],
 {
 
 	int i = BUFF_SIZE - 2;
-	unsigned long int num = va_arg(types, unsigned long int);
-	unsigned long int init_num = num;
+	unsigned long int sum = va_arg(types, unsigned long int);
+	unsigned long int init_num = sum;
 
 	UNUSED(width);
 
-	num = convert_size_unsgnd(num, size);
+	sum = convert_size_unsgnd(sum, size);
 
-	if (num == 0)
+	if (sum == 0)
 		buffer[i--] = '0';
 
 	buffer[BUFF_SIZE - 1] = '\0';
 
-	while (num > 0)
+	while (sum > 0)
 	{
-		buffer[i--] = (num % 8) + '0';
-		num /= 8;
+		buffer[i--] = (sum % 8) + '0';
+		sum /= 8;
 	}
 
 	if (flags & F_HASH && init_num != 0)
@@ -131,22 +131,22 @@ int print_hexa(va_list types, char map_to[], char buffer[],
 	int flags, char flag_ch, int width, int precision, int size)
 {
 	int i = BUFF_SIZE - 2;
-	unsigned long int num = va_arg(types, unsigned long int);
-	unsigned long int init_num = num;
+	unsigned long int sum = va_arg(types, unsigned long int);
+	unsigned long int init_num = sum;
 
 	UNUSED(width);
 
-	num = convert_size_unsgnd(num, size);
+	sum = convert_size_unsgnd(sum, size);
 
-	if (num == 0)
+	if (sum == 0)
 		buffer[i--] = '0';
 
 	buffer[BUFF_SIZE - 1] = '\0';
 
-	while (num > 0)
+	while (sum > 0)
 	{
-		buffer[i--] = map_to[num % 16];
-		num /= 16;
+		buffer[i--] = map_to[sum % 16];
+		sum /= 16;
 	}
 
 	if (flags & F_HASH && init_num != 0)
